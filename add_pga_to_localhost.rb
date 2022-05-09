4a5a677069c02ecd70825a3d3ec6e66de55caf86
Minh Chung <minh@minhchung.com>
2022-05-09 14:35:28 -0700

    add chef script

---
+++ add_pga_to_localhost.rb
@@ -0,0 +1,39 @@
+hostsfile_entry '199.36.153.8' do
+  hostname  'private.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.9' do
+  hostname  'private.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.10' do
+  hostname  'private.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.11' do
+  hostname  'private.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.4' do
+  hostname  'restricted.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.5' do
+  hostname  'restricted.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.6' do
+  hostname  'restricted.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.7' do
+  hostname  'restricted.googleapis.com'
+  action    :create_if_missing
+end
