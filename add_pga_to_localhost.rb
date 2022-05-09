9f18b4fd511829ca1f12f6dd4da721f36d75f4ae
Minh Chung <minh@minhchung.com>
2022-05-09 15:15:10 -0700

    add all apis entries

--- add_pga_to_localhost.rb
+++ add_pga_to_localhost.rb
@@ -1,39 +1,119 @@
 hostsfile_entry '199.36.153.8' do
-  hostname  'private.googleapis.com'
+  hostname  'vmmigration.googleapis.com'
   action    :create_if_missing
 end
 
 hostsfile_entry '199.36.153.9' do
-  hostname  'private.googleapis.com'
+  hostname  'vmmigration.googleapis.com'
   action    :create_if_missing
 end
 
 hostsfile_entry '199.36.153.10' do
-  hostname  'private.googleapis.com'
+  hostname  'vmmigration.googleapis.com'
   action    :create_if_missing
 end
 
 hostsfile_entry '199.36.153.11' do
-  hostname  'private.googleapis.com'
+  hostname  'vmmigration.googleapis.com'
   action    :create_if_missing
 end
 
-hostsfile_entry '199.36.153.4' do
-  hostname  'restricted.googleapis.com'
+hostsfile_entry '199.36.153.8' do
+  hostname  'servicemanagement.googleapis.com'
   action    :create_if_missing
 end
 
-hostsfile_entry '199.36.153.5' do
-  hostname  'restricted.googleapis.com'
+hostsfile_entry '199.36.153.9' do
+  hostname  'servicemanagement.googleapis.com'
   action    :create_if_missing
 end
 
-hostsfile_entry '199.36.153.6' do
-  hostname  'restricted.googleapis.com'
+hostsfile_entry '199.36.153.10' do
+ hostname  'servicemanagement.googleapis.com'
   action    :create_if_missing
 end
 
-hostsfile_entry '199.36.153.7' do
-  hostname  'restricted.googleapis.com'
+hostsfile_entry '199.36.153.11' do
+  hostname  'servicemanagement.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.8' do
+  hostname  'servicecontrol.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.9' do
+  hostname  'servicecontrol.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.10' do
+ hostname  'servicecontrol.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.11' do
+  hostname  'servicecontrol.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.8' do
+  hostname  'iam.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.9' do
+  hostname  'iam.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.10' do
+ hostname  'iam.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.11' do
+  hostname  'iam.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.8' do
+  hostname  'cloudresourcemanager.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.9' do
+  hostname  'cloudresourcemanager.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.10' do
+ hostname  'cloudresourcemanager.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.11' do
+  hostname  'cloudresourcemanager.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.8' do
+  hostname  'compute.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.9' do
+  hostname  'compute.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.10' do
+ hostname  'compute.googleapis.com'
+  action    :create_if_missing
+end
+
+hostsfile_entry '199.36.153.11' do
+  hostname  'compute.googleapis.com'
   action    :create_if_missing
 end
